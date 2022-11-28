0488a147f4b87e5b724cbe6aff4ef682e103f12a
Yaroslav <shpaklviv@gmail.com>
2022-11-28 15:14:06 +0200

    add summ word

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+import string
 
 def home(request):
 	return render(request, 'home.html')
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
+	result_word = str(len(user_text.split()))
 	return render(request, 'reverse.html', {'usertext':user_text,
-		'reversedtext':reversed_text})
+		'reversedtext':reversed_text,'resultword':result_word})
